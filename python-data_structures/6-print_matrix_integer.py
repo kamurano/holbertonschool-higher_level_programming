f805c937a5e8927e606e941fd8c286d52d943abd
kamurano <kamrannn.mk@gmail.com>
2024-05-24 18:29:53 +0400

    fix task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for num in row:
-            if num != len(row):
+        for i, num in enumerate(row):
+            if i != len(row):
                 print("{:d}".format(num), end=" ")
             else:
                 print("{:d}".format(num), end="")
