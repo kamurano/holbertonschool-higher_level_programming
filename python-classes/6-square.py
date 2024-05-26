bcb97a49957838325ab9c9cd1fbcb458cdccd085
kamurano <kamrannn.mk@gmail.com>
2024-05-26 19:43:38 +0400

    classes python

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -52,5 +52,7 @@ class Square:
         if self.__size == 0:
             print()
         else:
-            for _ in range(self.__size):
-                print("#" * self.__size)
+            for i in range(self.__position[1]):
+                print()
+            for i in range(self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)
