163b074c5be86da3d234e73035d25d005291ba79
kamurano <kamrannn.mk@gmail.com>
2024-05-26 19:44:53 +0400

    classes python

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -33,11 +33,11 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
-            
+
     @property
     def position(self):
         return self.__position
-    
+
     @position.setter
     def position(self, value):
         if not isinstance(value, tuple) or len(value) != 2 or \
@@ -46,7 +46,7 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
-            
+
     def my_print(self):
         """Print square with #"""
         if self.__size == 0:
