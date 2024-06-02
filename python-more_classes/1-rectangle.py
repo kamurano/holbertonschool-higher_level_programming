838a89681d0f5e86e9bd1408fc42ac8bdfb23bbd
kamurano <kamrannn.mk@gmail.com>
2024-06-02 17:07:37 +0400

    task 1 fix

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -6,8 +6,8 @@ class Rectangle:
     """Class that defines a rectangle"""
     def __init__(self, width=0, height=0):
         """Method to initialize the rectangle"""
-        self.__width = 0
-        self.__height = 0
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
