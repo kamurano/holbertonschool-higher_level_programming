46dd0b0f12aa0b9f86ce2c90051a465ef3a87f89
kamurano <kamrannn.mk@gmail.com>
2024-06-02 17:57:43 +0400

    task 7 pycodestyle

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -55,8 +55,8 @@ class Rectangle:
         """Method to return the string representation of the rectangle"""
         if self.width == 0 or self.height == 0:
             return ""
-        l = [f"{self.print_symbol}" * self.width for i in range(self.height)]
-        return "\n".join(l)
+        listo = [f"{self.print_symbol}" * self.width for i in range(self.height)]
+        return "\n".join(listo)
 
     def __repr__(self):
         """Method to return the string representation of the rectangle"""
