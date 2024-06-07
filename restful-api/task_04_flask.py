eb3dd57adacae520f120280216a1ab77a97cd128
kamurano <kamrannn.mk@gmail.com>
2024-06-08 03:04:20 +0400

    restfull task 4 fix

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -20,7 +20,7 @@ def add_user():
         user = request.get_json()
         if user is None:
             return jsonify({"error": "No data provided"}), 400
-        if user["username"] is None:
+        if user["username"] not in user.keys():
             return jsonify({"error": "No username provided"}), 400
         if user['username'] in all_users.keys():
             return jsonify({"error": "User already exists"}), 400
