346ac96e81d61ef74a6ce766d7df6ea908355635
kamurano <kamrannn.mk@gmail.com>
2024-07-17 03:49:43 +0400

    task 0 fix

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -4,12 +4,14 @@ import os
 
 def generate_invitations(template_content, attendees):
     x = 1
+    if attendees == []:
+        with open(f"output_{x}.txt", 'w') as f:
+            f.write(template_content)
     for attendee in attendees:
         if not (os.path.exists(f"output_{x}.txt")):
             return
         with open(f"output_{x}.txt", 'w') as f:
             try:
-
                 invitation = template_content.format(**attendee)
                 f.write(invitation)
                 x += 1
