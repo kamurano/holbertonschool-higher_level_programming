88b5d53f75fb1fc6c4a7bbe216dfa05f99e3b86d
striker06 <105947149+striker06@users.noreply.github.com>
2024-07-17 22:16:00 +0400

    task 2 fix

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 @app.route('/items')
 def items():
     with open('items.json') as f:
-        items = load(f)["items"]
+        data = load(f)
+        items = data.get('items') or []
+        
     return render_template('items.html', items=items)
 
 app.run(port=5000, debug=True)
