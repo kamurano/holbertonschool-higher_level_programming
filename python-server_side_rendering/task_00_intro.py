cba6deb2a9ef7a0754ffad0ba3e6a7069259daac
striker06 <105947149+striker06@users.noreply.github.com>
2024-07-17 21:40:11 +0400

    task 0 fix

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -3,22 +3,36 @@ import os
 
 
 def generate_invitations(template_content, attendees):
+    """Function for generating invitations"""
+
+    if not template_content:
+        print("ERROR: template cannot be empty")
+        return
+
     if not attendees:
-        return "No data provided, no output files generated."
-    
-    x = 1
-    for attendee in attendees:
-        if not os.path.exists(f"output_{x}.txt"):
-            return "Template is empty, no output files generated."
-        with open(f"output_{x}.txt", 'w') as f:
-            # try:
-            attendee = {k: v if v is not None else "N/A" for k, v in attendee.items()}
-            invitation = template_content.format(**attendee)
-            f.write(invitation)
-            x += 1
-            # except Exception as e:
-                # print(e)
-                # continue
+        print("ERROR: attendees_list cannot be empty")
+        return
+
+    if not isinstance(template_content, str):
+        print("ERROR: template must be a string")
+        return
+
+    if (not isinstance(attendees, list) or
+            not all(isinstance(item, dict) for item in attendees)):
+        print("ERROR: attendees_list must be a list of dictionaries")
+        return
+
+    for index, attendee in enumerate(attendees, start=1):
+        template_schema = template_content
+        for key in ['name', 'event_title', 'event_date', 'event_location']:
+            placeholder = "{" + f"{key}" + "}"
+            value = attendee.get(key) or "N/A"
+            template_schema = template_schema.replace(placeholder, value)
+        if not os.path.exists(f"output_{index}.txt"):
+            with open(f"output_{index}.txt", "w") as file:
+                file.write(template_schema)
+        else:
+            print("ERROR: file already exists")
             
             
 if __name__ == "__name__":
